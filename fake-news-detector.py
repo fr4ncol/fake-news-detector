2d3a0f05ade67126389e87c751ccec2d20e82fe2
Franciszek Plisz <franciszekplisz@protonmail.com>
2024-12-03 18:53:12 +0100

    Code cleanup. Added .gitignore file

--- fake-news-detector.py
+++ fake-news-detector.py
@@ -3,13 +3,11 @@ import requests
 from mistralai import Mistral
 
 app = Flask(__name__)
-app.secret_key = "supersecretkey"  # Ustaw klucz do sesji Flaska
+app.secret_key = "supersecretkey"  
 
-# Inicjalizacja klienta Mistral
 model = "mistral-large-latest"
-client = Mistral(api_key='B4HRyk4lAmuY42FNd2oafVjWEFn91mjo')
+client = Mistral(api_key='LLyqibiPrFeMPvNQvdvlwr6lLGgzCrS2')
 
-# Funkcja do pobierania tekstu z tweeta
 def get_tweet_text(url):
     try:
         post_id = url.split("/")[-1]
@@ -18,15 +16,24 @@ def get_tweet_text(url):
         data = response.json()
         return data.get("text", "No text found.")
     except Exception as e:
-        return f"Błąd: {e}"
+        return f"Error: {e}"
 
-# Funkcja do uzyskania odpowiedzi z modelu Mistral
 def get_mistral_response(tweet_text, language):
     try:
         if language == "en":
-            content = f"Classify whether the statement is true or false: {tweet_text}"
+            content = """
+                      Determine whether the following statement is "True" or "False":
+                      Statement: {statement}
+                      Label:
+                      """
+            content = content.format(tweet_text=tweet_text)
         else:
-            content = f"Sklasyfikuj, czy podane stwierdzenie jest prawdziwe czy fałszywe: {tweet_text}"
+            content = """
+                      Określ, czy poniższe stwierdzenie jest "Prawdziwe" czy "Fałszywe":
+                      Stwierdzenie: {statement}
+                      Klasyfikacja:  
+                      """
+            content = content.format(tweet_text=tweet_text)
 
         response = client.chat.complete(
             model=model,
@@ -43,7 +50,6 @@ def get_mistral_response(tweet_text, language):
 
 @app.route("/", methods=["GET", "POST"])
 def index():
-    # Ustawienie domyślnego języka, jeśli nie istnieje w sesji
     if "language" not in session:
         session["language"] = "pl"
 
@@ -52,9 +58,7 @@ def index():
     if request.method == "POST":
         tweet_url = request.form.get("tweet_url")
         if tweet_url:
-            # Pobieranie tekstu tweeta
             tweet_text = get_tweet_text(tweet_url)
-            # Uzyskiwanie odpowiedzi od Mistral
             mistral_response = get_mistral_response(tweet_text, language)
             return render_template("index.html", tweet_text=tweet_text, tweet_url=tweet_url, mistral_response=mistral_response, language=language)
         else:
